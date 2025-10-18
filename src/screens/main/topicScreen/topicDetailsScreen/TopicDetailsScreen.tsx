28d542432c433ae1bde705ffeed13f69fad3416a
Eugenio Duarte <eugenioduartesilva@gmail.com>
2025-10-18 18:03:27 +0100

    feat: test

---
+++ src/screens/main/topicScreen/topicDetailsScreen/TopicDetailsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const TopicDetailsScreen = () => {
+  return (
+    <View>
+      <Text>TopicDetailsScreen</Text>
+    </View>
+  )
+}
+
+export default TopicDetailsScreen
