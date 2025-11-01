2837c75b66c013a87e9986d2333be5edfc65306b
Eugenio Silva <eugenioduartesilva@gmail.com>
2025-11-01 03:14:01 +0000

    feat:teste

---
+++ src/screens/main/topic/components/TopicCard/TopicCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const TopicCard = () => {
+  return (
+    <View>
+      <Text>TopicCard</Text>
+    </View>
+  )
+}
+
+export default TopicCard
